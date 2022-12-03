5fe9068712dc6a50eee0ccfbefe2173eb74cff4a
Maria Volkova <safety-team.ru@yandex.ru>
2022-12-03 23:58:36 +0700

    fix: router for auth user

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,16 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import './App.module.css';
 import AppRouter from './components/app-router/app-router';
+import { getUser } from './services/actions/auth';
 
 
 function App() {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getUser())
+  }, [])
 
   return (
     <AppRouter />
